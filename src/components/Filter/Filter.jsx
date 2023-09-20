7db6eb3cb964af7cf1eb3e90c44ab0ca48bb28e9
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-09-20 15:23:42 +0200

    add start files src, install dependences of project

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import { FilterStyled, Input, Label } from './Filter.styled';
+
+export const Filter = ({ value, onChange }) => {
+  return (
+    <FilterStyled>
+      <Label>Find contsct by name</Label>
+      <Input type="text" name="filter" value={value} onChange={onChange} />
+    </FilterStyled>
+  );
+};
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
